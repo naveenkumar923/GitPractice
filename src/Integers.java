c3b974328c2bbaf6de88c337a3d02ab16f9157db
Naveen <naveenkumar923@gmail.com>
2023-01-16 17:07:20 -0500

    trying to create a merge conflict

--- src/Integers.java
+++ src/Integers.java
@@ -3,7 +3,7 @@ public class Integers {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i=0;i<=100;i++) {
+		for(int i=0;i<=200;i++) {
 		System.out.println(i);
 		}
 	}
