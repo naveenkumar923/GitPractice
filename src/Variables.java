781a2155a5d41888750f7f668ec9e92d2981f8a9
Naveen <naveenkumar923@gmail.com>
2023-01-16 16:38:57 -0500

    switched to orsonata branch from selenium practice original

--- src/Variables.java
+++ src/Variables.java
@@ -21,6 +21,7 @@ public class Variables {
 		System.out.println("learning git");
 		System.out.println("practicing commit and push in git");
 		System.out.println("created new branch orsonata");
+		System.out.println("switched to branch orsonata");
 
 	}
 
